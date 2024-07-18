7d76227c5ac571a39e36416cbb7b953f8506a04e
adavillacarlos <adavillacarlos@outlook.com>
2024-07-18 23:12:08 +0800

    added shadcn button

--- client/lightnovels/src/App.tsx
+++ client/lightnovels/src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 
   return (
     <>
       <h1 className="font-bold text-lg">Hello</h1>
+      <Button>Click</Button> 
     </>
   )
 }
